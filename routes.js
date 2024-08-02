e08d4430b4d2af2e046e3a1fc26246130202a392
hilkyapple <hildakyakuwa@gmail.com>
2024-08-02 10:36:22 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-//CRUD
-// GET METHOD
+                //CRUD
+// GET METHOD(all posts)
 
 router.get('/posts',(req,res)=>{
-    res.send('posts page')
+   // res.send('posts page')
+
+   res.json(
+    {msg:'Get all posts'}
+   )
 })
+router.get('/posts/:id',(req,res)=>{
+    // res.send('posts page')
+ 
+    res.json(
+     {msg:'Get a specific post'}
+    )
+ })
+
 module.exports=router
